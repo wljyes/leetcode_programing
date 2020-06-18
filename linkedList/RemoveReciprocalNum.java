057e988716df8f8c42523b9cf3f6cc337752dcbc
wljyes <1757280079@qq.com>
2020-06-18 20:47:26 +0800

    add double pointer solution

--- linkedList/RemoveReciprocalNum.java
+++ linkedList/RemoveReciprocalNum.java
@@ -6,7 +6,7 @@ public class RemoveReciprocalNum {
         ListNode one = new ListNode(1);
         ListNode two = new ListNode(2);
         one.next = two;
-        ListNode head = new RemoveReciprocalNum().removeNthFromEnd(one, 2);
+        ListNode head = new RemoveReciprocalNum().removeByDoublePointer(one, 2);
         System.out.println(head.val);
     }
 
@@ -30,6 +30,29 @@ public class RemoveReciprocalNum {
         }
         cursor.next = cursor.next.next;
         cursor.next.next = null;
+        return dummyNode.next;
+    }
+
+    public ListNode removeByDoublePointer(ListNode head, int n) {
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        //两个指针
+        ListNode cursor = dummyNode;
+        ListNode cursor2 = dummyNode;
+        //计数第一个指针走的步数
+        int count = 0;
+        while (cursor.next != null) {
+            //分开两个指针的距离，之后当第一个指针走到最后一个时，第二个指针就在倒数第n个的前面
+            if (count < n) { 
+                cursor = cursor.next;
+                count++;
+            } else {
+                cursor = cursor.next;
+                cursor2 = cursor2.next;
+            }
+        }
+        //删除倒数第n个节点，即第二个指针后的节点
+        cursor2.next = cursor2.next.next;
         return dummyNode.next;
     }
 }
