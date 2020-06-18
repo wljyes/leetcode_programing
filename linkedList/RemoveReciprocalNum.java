14c9f4e0a96c22ac1890d980ff9754e96bc48dc2
wljyes <1757280079@qq.com>
2020-06-18 14:53:36 +0800

    add dummyNode

--- linkedList/RemoveReciprocalNum.java
+++ linkedList/RemoveReciprocalNum.java
@@ -2,11 +2,20 @@ package linkedList;
 
 public class RemoveReciprocalNum {
     
+    public static void main(String[] args) {
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        one.next = two;
+        ListNode head = new RemoveReciprocalNum().removeNthFromEnd(one, 2);
+        System.out.println(head.val);
+    }
 
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
         int length = 0;
-        ListNode cursor = head;
+        ListNode cursor = dummyNode;
         //一次扫描计算链表长度
         while (cursor != null) {
             cursor = cursor.next;
@@ -14,17 +23,14 @@ public class RemoveReciprocalNum {
         }
         //计算倒数的正数位置
         int index = length - n;
-        cursor = head;        
+        cursor = dummyNode;        
         //移动到要删除的元素前面一个位置
         for (int i = 0; i < index - 1; i++) {
             cursor = cursor.next;
         }
-        //考虑要删除的是头节点
-        if (index == 0)
-            head = head.next;
-        else 
-            cursor.next = cursor.next.next;
-        return head;
+        cursor.next = cursor.next.next;
+        cursor.next.next = null;
+        return dummyNode.next;
     }
 }
 
