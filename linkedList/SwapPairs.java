44d6db3b46493b00be90cb3fc623eab786a2cf8d
wljyes <1757280079@qq.com>
2020-06-19 10:54:55 +0800

    add recursive solution

--- linkedList/SwapPairs.java
+++ linkedList/SwapPairs.java
@@ -1,7 +1,5 @@
 package linkedList;
 
-import com.mysql.cj.x.protobuf.MysqlxNotice.Frame;
-
 public class SwapPairs {
     
     public ListNode swapPairs(ListNode head) {
@@ -25,5 +23,24 @@ public class SwapPairs {
                 second = first.next;
         }
         return dummy.next;
+    }
+
+    //两两交换递归
+    public ListNode swapPairsRecusive(ListNode head) {
+        //递归出口
+        if (head == null || head.next == null) 
+            return head;
+        
+        //交换这两个节点后面的两个节点，并拿到返回的头节点
+        ListNode node = swapPairsRecusive(head.next.next);
+        //交换后的头节点实际上是原来的第二个节点
+        ListNode second = head.next;
+
+        //交换当前两节点，并连上后面的节点
+        head.next.next = head;
+        head.next = node;
+
+        //返回交换后的头节点
+        return second;
     }
 }
